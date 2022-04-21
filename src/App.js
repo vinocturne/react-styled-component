6158baf5d8b759ce183dab1fce6d12739eda07ae
Ghunhee Shin <hago@rocketstaff.com>
2022-04-21 11:44:44 +0900

    Feat(사용법) : "추가 styled-components의 사용법"
    styled-components의 사용법
    styled.사용하고자하는 태그명 후 백틱(``)을 활용해 백틱 내부에 일반적인 css를 입력해 주면 된다.
    div의 늪에서 해방될 수 있고, 각 태그들이 매우 깔끔하게 정의된다.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
+
+//style.(사용하고자 하는 태그이름)에 백틱(`)을 이용해 css를 적용할 수 있다.
+const Father = styled.div`
+  display: flex;
+`
+
+const BoxOne = styled.div`
+  background-color: teal;
+  width: 100px;
+  height: 100px;
+`
+
+const BoxTwo = styled.div`
+  background-color: tomato;
+  width: 100px;
+  height: 100px;
+`
+
+const Text = styled.span`
+  color: white;
+`
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    //styled component를 사용하면 div의 늪에서 자유로워질 수 있다.
+    //요소검사에서 class명은 styled-component에서 임의로 정해져 적용된다.
+    <Father>
+      <BoxOne>
+        <Text>Hello</Text>
+      </BoxOne>
+      <BoxTwo />
+    </Father>
+    
+    //가장 직관적이나 괄호를 각각 열어주어야하고 javascript 코드 방식으로 스타일을 지정해주어야 하는 단점이 있다.
+    // <div style={{display: "flex"}}>
+    //   <div style={{backgroundColor:"teal", width: 100, height: 100}}></div>
+    //   <div style={{backgroundColor:"tomato", width: 100, height: 100}}></div>
+    // </div>
   );
 }
 
